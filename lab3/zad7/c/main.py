96e4f1af4f1c10b5ec69558bf4d3ec5823c1c301
Łukasz Kulpaczyński <lukasz18kulpaczynski@gmail.com>
2025-11-25 18:09:04 +0100

    aktualne

---
+++ lab3/zad7/c/main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Wczytaj obraz w skali szarości
+img = cv2.imread("RezydencjaDiabla.png", cv2.IMREAD_GRAYSCALE)
+
+alpha = -1/3
+
+# Hiperbolizacja histogramu
+g = img.astype(np.float32)
+hyper = 255 / (1 - np.exp(-alpha)) * (1 - np.exp(-alpha * g / 255.0))
+hyper = np.clip(hyper, 0, 255).astype(np.uint8)
+
+# Zapisz obraz wynikowy
+cv2.imwrite("RezydencjaDiabla_hyper.png", hyper)
+
+# Pokaż obrazy
+plt.figure(figsize=(12,5))
+plt.subplot(1,2,1)
+plt.imshow(img, cmap='gray')
+plt.title('Oryginalny obraz')
+plt.axis('off')
+
+plt.subplot(1,2,2)
+plt.imshow(hyper, cmap='gray')
+plt.title('Po hiperbolizacji (α = -1/3)')
+plt.axis('off')
+plt.show()
+
+# Histogram hiperbolicznego obrazu
+plt.figure()
+plt.hist(hyper.ravel(), bins=256, range=(0,255), color='black')
+plt.title('Histogram po hiperbolizacji')
+plt.xlabel('Wartości szarości')
+plt.ylabel('Liczba pikseli')
+plt.savefig("hist_hyper.png")   # automatyczny zapis histogramu
+plt.show()
